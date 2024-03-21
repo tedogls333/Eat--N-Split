a770908f2d070761472791afa506caf1854e09a6
tedogls333 <tedogls3@gmail.com>
2024-03-21 20:59:11 +0100

    Eat&split

--- 04-eat/src/App.js
+++ 04-eat/src/App.js
@@ -75,6 +75,7 @@ export default function App() {
       </div>
       {selectedFriend && (
         <FormSplitBill
+          key={selectedFriend.id}
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
         />
